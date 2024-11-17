0612b8880b3fd214fb4691ee996ca2db332bb242
maayan gotfrid <120467015+maayangotfrid@users.noreply.github.com>
2024-11-17 09:43:56 +0200

    Update download_video.py

--- functions/download_video.py
+++ functions/download_video.py
@@ -2,12 +2,14 @@ import os
 from flask import send_from_directory
 
 def download_video(filename):
-    # נשתמש בנתיב מלא כדי לשלוח את הקובץ להורדה
+    # ניצור את הנתיב המלא של הקובץ
     try:
         file_path = os.path.join(os.getcwd(), 'Tiktok_download_files', filename)
+        
+        # אם הקובץ קיים, נשלח אותו להורדה
         if os.path.exists(file_path):
             return send_from_directory(directory=os.path.dirname(file_path), filename=filename, as_attachment=True)
         else:
             return "File not found, please try again."
-    except FileNotFoundError:
-        return "File not found, please try again."
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
