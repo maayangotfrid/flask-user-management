574e05f9c44d964e4ccaa4bd5e0c24623576b3a8
maayan gotfrid <120467015+maayangotfrid@users.noreply.github.com>
2024-11-17 09:41:46 +0200

    Update update_user.py

--- functions/update_user.py
+++ functions/update_user.py
@@ -1,44 +1,23 @@
-from flask import request
-import psycopg2
-from psycopg2 import sql
+from functions.db_connection import get_db_connection
 
-def update_user_in_db(user_id):
-    # קבלת הנתונים מהטופס
-    name = request.form.get('name')
-    email = request.form.get('email')
-    phone = request.form.get('phone')
-    role = request.form.get('role')
-    password = request.form.get('password')
-    valid = request.form.get('valid')
-    active = request.form.get('active')
-    image_url = request.form.get('image_url')
-
-    # חיבור למסד נתונים
-    connection = psycopg2.connect(
-        host='dpg-css42c1u0jms73e5mel0-a.frankfurt-postgres.render.com',
-        port=5432,
-        database='mgproject',
-        user='maayan',
-        password='BowLYRTI3H9xpayZixjVTFWM0malIzzj'
-    )
+def update_user_in_db(user_id, name, email, phone, role, password, valid, active, image_url):
+    # חיבור למסד הנתונים
+    connection = get_db_connection()
     cursor = connection.cursor()
 
     try:
-        # יצירת פקודת SQL לעדכון המידע
-        update_query = sql.SQL("""
-            UPDATE users
-            SET name = %s, email = %s, phone = %s, role = %s, password = %s, 
-                valid = %s, active = %s, image_url = %s
+        # ביצוע פקודת SQL לעדכון המשתמש
+        cursor.execute('''
+            UPDATE users 
+            SET name = %s, email = %s, phone = %s, role = %s, password = %s, valid = %s, active = %s, image_url = %s 
             WHERE id = %s
-        """)
-
-        # הרצת הפקודה עם הערכים החדשים
-        cursor.execute(update_query, (name, email, phone, role, password, valid, active, image_url, user_id))
+        ''', (name, email, phone, role, password, valid, active, image_url, user_id))
         connection.commit()
 
         return "User updated successfully"
     except Exception as e:
-        connection.rollback()  # אם יש שגיאה, נעשה rollback של השינויים
+        # אם קרתה שגיאה, נבצע rollback
+        connection.rollback()
         return f"Error updating user: {str(e)}"
     finally:
         cursor.close()
